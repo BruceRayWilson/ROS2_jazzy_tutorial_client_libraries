7f635a29b6003e1d26330ad5ab4ee510eee6f13f
Bruce Ray Wilson <BruceRayWilson42@gmail.com>
2025-01-01 17:28:16 -0600

    touch

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
-docker build -t ros2_diff_drive_jazzy \
-    --build-arg USERNAME=$(whoami) \
-    --build-arg USER_UID=$(id -u) \
-    --build-arg USER_GID=$(id -g) .
+docker build -t ros2_diff_drive_jazzy .
